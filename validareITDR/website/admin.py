ab7948c968a3042207d0efb146e2684bdb03ad1d
Vali Manu <manu_vali85@yahoo.com>
2020-12-02 21:24:48 +0200

    db model

--- validareITDR/website/admin.py
+++ validareITDR/website/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Store, GroupStore, Product, Campaign
 
-# Register your models here.
+admin.site.register(Store)
+admin.site.register(GroupStore)
+admin.site.register(Product)
+admin.site.register(Campaign)
--- validareITDR/website/models.py
+++ validareITDR/website/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Store(models.Model):
+    store_name = models.CharField(max_length=30, verbose_name='Denumire')
+    store_address = models.CharField(max_length=40, verbose_name='Adresa')
+    store_active = models.BooleanField(verbose_name='Activ', default=True)
+    group_store = models.ForeignKey('GroupStore', on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        ordering = ['store_name']
+
+    def __str__(self):
+        return self.store_name
+
+
+class GroupStore(models.Model):
+    group_store = models.CharField(max_length=20, verbose_name='Grup Magazin')
+
+    def __str__(self):
+        return self.group_store
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=20, verbose_name='Denumire')
+    url_img = models.CharField(max_length=200, verbose_name='URL Img')
+    product_active = models.BooleanField(verbose_name='Activ', default=True)
+
+    class Meta:
+        ordering = ['product_name']
+
+    def __str__(self):
+        return self.product_name
+
+
+class Campaign(models.Model):
+    campaign_name = models.CharField(max_length=50, verbose_name='Denumire')
+    campaign_start_date = models.DateField(verbose_name='Data Start')
+    campaign_end_date = models.DateField(verbose_name='Data Sfarsit')
+    campaign_active = models.BooleanField(default=False, verbose_name='Activ')
+    product = models.ManyToManyField(Product)
+
+    class Meta:
+        ordering = ['campaign_name']
+
+    def __str__(self):
+        return self.campaign_name
